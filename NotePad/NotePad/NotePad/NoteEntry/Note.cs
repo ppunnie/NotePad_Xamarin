663db2d54b25f78204dd797ade75910455317439
TanPun <tanpun890@gmail.com>
2023-09-23 17:51:25 +0700

    update

---
+++ NotePad/NotePad/NotePad/NoteEntry/Note.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+
+namespace NotePad.NoteEntry
+{
+    public class Notes
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; } = "";
+
+        public DateTime Date { get; set; }
+
+        public bool IsChecked { get; set; }
+
+        public string HexColor { get; set; } = "#000000";
+        public double Size { get; set; } = 12;
+
+    }
+}
